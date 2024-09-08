3363af9cabaab7990858b388919a834781589cd7
yehudit-rosenblum <111yehudit@gmail.com>
2024-09-08 18:55:43 +0300

    fix matSelect -not allow city already selected in takeOff to show in landing

--- frontend/src/app/flight-form/flight-form.component.ts
+++ frontend/src/app/flight-form/flight-form.component.ts
@@ -1,3 +1,142 @@
+// import { Component, OnInit } from '@angular/core';
+// import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+// import { ActivatedRoute } from '@angular/router';
+// import { FlightService } from '../flight.service';
+// import { CommonModule, NgFor } from '@angular/common';
+// import { MatInputModule } from '@angular/material/input';
+// import { MatFormFieldModule } from '@angular/material/form-field';
+// import { MatSelectModule } from '@angular/material/select';
+// import { MatButtonModule } from '@angular/material/button';
+// import { MatDatepickerModule } from '@angular/material/datepicker';
+// import { MatNativeDateModule } from '@angular/material/core';
+// import { RouterModule } from '@angular/router';
+// import {MatAutocompleteModule} from '@angular/material/autocomplete';
+// import { Airport } from '../models/flight.model';
+// import { map, Observable, startWith } from 'rxjs';
+
+
+// @Component({
+//   selector: 'app-flight-form',
+//   templateUrl: './flight-form.component.html',
+//   styleUrls: ['./flight-form.component.scss'],
+//   imports: [CommonModule, ReactiveFormsModule,  MatInputModule,
+//     MatFormFieldModule,
+//     MatSelectModule,
+//     MatButtonModule,
+//     MatDatepickerModule,
+//     MatNativeDateModule, RouterModule, MatAutocompleteModule, FormsModule,   NgFor],
+//   standalone: true
+// })
+// export class FlightFormComponent implements OnInit {
+//   flightForm: FormGroup;
+//   flightId: string | null = null;
+//   airports: Airport[] = [];
+//   isSaved: boolean = false;  
+//   filteredAirports$: Observable<Airport[]> | undefined = new Observable();
+//   landFilteredAirports$: Observable<Airport[]> | undefined = new Observable(); 
+  
+//   constructor( private fb: FormBuilder, private route: ActivatedRoute, private flightService:FlightService){
+//     this.flightForm = this.fb.group({
+//       flightNumber: ['', Validators.required],
+//       takeOffAirport: ['', Validators.required],
+//       landingAirport: ['', Validators.required],
+//       takeoffTime: ['', Validators.required],
+//       landingTime: ['', Validators.required],
+//       status: ['', Validators.required]
+//     });
+    
+//     this.filteredAirports$ = this.flightForm.get('takeOffAirport')?.valueChanges.pipe(
+//       startWith(''),
+//       map(airport => (airport ? this.filterAirports(airport) : this.airports.slice())),
+//     );
+
+//     this.landFilteredAirports$ = this.flightForm.get('landingAirport')?.valueChanges.pipe(
+//       startWith(''),
+//       map(airport => (airport ? this.filterAirports(airport) : this.airports.slice())),
+//     );
+//   }
+
+//   ngOnInit(): void {
+//     debugger
+    
+
+//     //מקבל את כל השדות תעופה
+//     this.flightService.getAirPorts().subscribe(
+//       res =>  this.airports = res);
+
+//       //כפתור שמירה דלוק
+//     this.isSaved=false;
+
+//   //לוקח את ה id
+//     this.flightId = this.route.snapshot.paramMap.get('id');
+//     //אם אכן הגיע
+//     if (this.flightId) {
+//         this.loadFlightDetails(this.flightId);
+//         this.flightForm.get('flightNumber')?.clearValidators();
+//         this.flightForm.get('flightNumber')?.updateValueAndValidity();
+//     }
+
+    
+//   }
+  
+  
+
+  
+//   loadFlightDetails(id: string) {
+//     this.flightService.getFlightById(id).subscribe(data => {
+//       this.flightForm.patchValue({
+//         flightNumber: data.flightNumber,
+//         takeoffTime: data.takeoffTime,
+//         landingTime: data.landingTime,
+//         status: data.status
+//       });
+//     });
+//   }
+
+//   saveFlight() {
+//     debugger
+//     this.isSaved=true;
+//     if (this.flightId) {
+//       this.flightService.updateFlight(this.flightId, this.flightForm.value).subscribe(
+//         response => {
+//       });
+//     } 
+//     else {
+//       this.flightService.createFlight(this.flightForm.value).subscribe(response => {
+//           console.log('Flight added successfully', response);
+//         },
+//         (error) => {
+//           console.error('Error adding flight:', error);
+//         }
+//       );
+//     }
+//   }
+
+//   public get takeOffReportsControl() : FormControl {
+//     return <FormControl>this.flightForm.get('takeOffAirport');
+//   }
+  
+//   public get landingReportsControl() : FormControl {
+//     return <FormControl>this.flightForm.get('landingAirport');
+//   }
+
+//   filterAirports(value: string | Airport): Airport[] {
+//     let filterValue: string = '';
+    
+//     // בדיקה שהערך הוא מחרוזת לפני השימוש ב-toLowerCase
+//     if (typeof value === 'string') {
+//       filterValue = value.toLowerCase();
+//     } else if (typeof value === 'object' && value !== null) {
+//       filterValue = value.name.toLowerCase();
+//     }
+  
+//     return this.airports.filter(airport => airport.name?.toLowerCase().includes(filterValue));
+//   }
+
+//   displayAirport(airport: Airport): string {
+//     return airport ? airport.name : '';
+//   }
+// }
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
@@ -10,32 +149,39 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { RouterModule } from '@angular/router';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { Airport } from '../models/flight.model';
 import { map, Observable, startWith } from 'rxjs';
-
 
 @Component({
   selector: 'app-flight-form',
   templateUrl: './flight-form.component.html',
   styleUrls: ['./flight-form.component.scss'],
-  imports: [CommonModule, ReactiveFormsModule,  MatInputModule,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatInputModule,
     MatFormFieldModule,
     MatSelectModule,
     MatButtonModule,
     MatDatepickerModule,
-    MatNativeDateModule, RouterModule, MatAutocompleteModule, FormsModule,   NgFor],
+    MatNativeDateModule,
+    RouterModule,
+    MatAutocompleteModule,
+    FormsModule,
+    NgFor
+  ],
   standalone: true
 })
 export class FlightFormComponent implements OnInit {
   flightForm: FormGroup;
   flightId: string | null = null;
   airports: Airport[] = [];
-  isSaved: boolean = false;  
+  isSaved: boolean = false;
   filteredAirports$: Observable<Airport[]> | undefined = new Observable();
-  landFilteredAirports$: Observable<Airport[]> | undefined = new Observable(); 
-  
-  constructor( private fb: FormBuilder, private route: ActivatedRoute, private flightService:FlightService){
+  landFilteredAirports$: Observable<Airport[]> | undefined = new Observable();
+
+  constructor(private fb: FormBuilder, private route: ActivatedRoute, private flightService: FlightService) {
     this.flightForm = this.fb.group({
       flightNumber: ['', Validators.required],
       takeOffAirport: ['', Validators.required],
@@ -44,36 +190,43 @@ export class FlightFormComponent implements OnInit {
       landingTime: ['', Validators.required],
       status: ['', Validators.required]
     });
-    
+
     this.filteredAirports$ = this.flightForm.get('takeOffAirport')?.valueChanges.pipe(
       startWith(''),
-      map(airport => (airport ? this.filterAirports(airport) : this.airports.slice())),
+      map(airport => (airport ? this.filterAirports(airport) : this.airports.slice()))
     );
 
-    this.landFilteredAirports$ = this.flightForm.get('landingAirport')?.valueChanges.pipe(
-      startWith(''),
-      map(airport => (airport ? this.filterAirports(airport) : this.airports.slice())),
-    );
+    // עדכון שדה הנחיתה כדי לוודא שהשדה שנבחר בהמראה לא יופיע כאן
+    this.flightForm.get('takeOffAirport')?.valueChanges.subscribe((selectedAirport: Airport | null) => {
+      if (selectedAirport && selectedAirport.name) {
+        this.landFilteredAirports$ = this.filteredAirports$?.pipe(
+          map(airports => airports.filter(airport => airport.name !== selectedAirport.name))
+        );
+      } else {
+        this.landFilteredAirports$ = this.filteredAirports$;
+      }
+    });
   }
 
   ngOnInit(): void {
-    debugger
+    // מקבל את כל שדות התעופה
     this.flightService.getAirPorts().subscribe(
-      res => 
-      this.airports = res);
+      res => this.airports = res
+    );
 
-    this.isSaved=false;
+    // כפתור שמירה דלוק
+    this.isSaved = false;
+
+    // לוקח את ה-id
     this.flightId = this.route.snapshot.paramMap.get('id');
+    // אם אכן הגיע
     if (this.flightId) {
-        this.loadFlightDetails(this.flightId);
-        this.flightForm.get('flightNumber')?.clearValidators();
-        this.flightForm.get('flightNumber')?.updateValueAndValidity();
+      this.loadFlightDetails(this.flightId);
+      this.flightForm.get('flightNumber')?.clearValidators();
+      this.flightForm.get('flightNumber')?.updateValueAndValidity();
     }
   }
-  
-  
 
-  
   loadFlightDetails(id: string) {
     this.flightService.getFlightById(id).subscribe(data => {
       this.flightForm.patchValue({
@@ -86,34 +239,31 @@ export class FlightFormComponent implements OnInit {
   }
 
   saveFlight() {
-    debugger
-    this.isSaved=true;
+    this.isSaved = true;
     if (this.flightId) {
-      this.flightService.updateFlight(this.flightId, this.flightForm.value).subscribe(
+      this.flightService.updateFlight(this.flightId, this.flightForm.value).subscribe();
+    } else {
+      this.flightService.createFlight(this.flightForm.value).subscribe(
         response => {
-      });
-    } 
-    else {
-      this.flightService.createFlight(this.flightForm.value).subscribe(response => {
           console.log('Flight added successfully', response);
         },
-        (error) => {
+        error => {
           console.error('Error adding flight:', error);
         }
       );
     }
   }
 
-  public get takeOffReportsControl() : FormControl {
+  public get takeOffReportsControl(): FormControl {
     return <FormControl>this.flightForm.get('takeOffAirport');
   }
-  
-  public get landingReportsControl() : FormControl {
+
+  public get landingReportsControl(): FormControl {
     return <FormControl>this.flightForm.get('landingAirport');
   }
 
   filterAirports(value: string | Airport): Airport[] {
-    let filterValue: string = '';
+    let filterValue = '';
     
     // בדיקה שהערך הוא מחרוזת לפני השימוש ב-toLowerCase
     if (typeof value === 'string') {
@@ -121,7 +271,7 @@ export class FlightFormComponent implements OnInit {
     } else if (typeof value === 'object' && value !== null) {
       filterValue = value.name.toLowerCase();
     }
-  
+
     return this.airports.filter(airport => airport.name?.toLowerCase().includes(filterValue));
   }
 
